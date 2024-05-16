ce00e35291a0c92431ddcaa1dc557a4d0e58e299
Matvey <59060848+redduvel@users.noreply.github.com>
2024-05-16 15:52:36 +0500

    1

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    # Запуск приложения с включенным режимом отладки и на определенном порту
-    app.run(debug=True)
+if __name__ == "__main__":
+    debug_mode = os.getenv('FLASK_DEBUG', 'False') == 'True'
+    app.run(debug=debug_mode)
