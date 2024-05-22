b71d63fc07fd4b8e34c2e7b42af4ed17127189b8
Matvey <59060848+redduvel@users.noreply.github.com>
2024-05-23 01:07:10 +0500

    set config

--- app/config/config.py
+++ app/config/config.py
@@ -3,5 +3,5 @@
 class Config:
     SECRET_KEY = 'stay_travel'
     JWT_SECRET_KEY = 'stay_travel'
-    MONGO_URI = 'mongodb+srv://admin:K9NsLtjlyJ0UWV5u@cluster0.ofbp53b.mongodb.net/test'
+    MONGO_URI = 'mongodb+srv://admin:admin@cluster0.ofbp53b.mongodb.net/test'
     
